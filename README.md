a525f8d3a92216c23a4c7a2cb9a2cd2d214c3c5e
Jessica Vaz <168509445+Jessicavazm@users.noreply.github.com>
2025-02-17 22:00:57 +1100

    feat: Basic express set, hello world return

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # t3iiw1-intro-to-mongoose
+
+### MongooseJS 
+- mongosh => mongo shell 
+- mongod => Mongo service daemon 
+- mongos => sharding or clustering for mongodb
+
+Start MongoDB Server sudo service mongod start sudo service mongod status sudo service mongod stop sudo service mongod restart
+
+Connect to the database
+
+Run the seed command: npm run seed
+Open the MongoDB shell: mongosh
+Show the list of databases: show dbs
+Connect to a particular database: use dbname
+db.posts.find()
+Saturday:
+Use MongoDB in Express Route
+Document Referencing and Populating
+Subdocuments
---
+++ src/server.js
@@ -0,0 +1,27 @@
+// Purpose of this file
+
+// Configure the server
+// Defining routes, middlewares, CORS, debug logger setups, db connections, etc.
+
+const express = require("express");
+
+// Instance of the express for configuration
+const app = express();
+
+
+
+// Server app config goes here...
+//
+// app.verb(path, callback);
+app.get("/", (request, response) => {
+    // response.send("<h1>Hello World!</h1>"); // Not Ideal
+
+    response.json({
+        message: "Hello World!"
+    })
+});
+
+// Export the server for use in other parts of the project
+module.exports = {
+    app
+}
